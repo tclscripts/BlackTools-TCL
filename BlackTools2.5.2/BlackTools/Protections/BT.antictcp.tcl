1fdfb8288299ad42a53ad18ee97f2e393378f404
Florian <office@tntradio.ro>
2018-01-18 08:46:33 +0200

    Add files via upload

---
+++ BlackTools2.5.2/BlackTools/Protections/BT.antictcp.tcl
@@ -0,0 +1,35 @@
+#########################################################################
+##          BlackTools - The Ultimate Channel Control Script           ##
+##                    One TCL. One smart Eggdrop                       ##
+#########################################################################
+##########################   ANTICTCP TCL   #############################
+#########################################################################
+##						                       ##
+##   BlackTools  : http://blacktools.tclscripts.net	               ##
+##   Bugs report : http://www.tclscripts.net/	                       ##
+##   GitHub page : https://github.com/tclscripts/BlackToolS-TCL-script ##
+##   Online Help : irc://irc.undernet.org/tcl-help 	               ##
+##                 #TCL-HELP / UnderNet                                ##
+##                 You can ask in english or romanian                  ##
+##					                               ##
+#########################################################################
+
+proc antictcp:protect {nick host hand chan keyword arg} {
+global black botnick
+if {![validchan $chan]} { return }
+if {[setting:get $chan antictcp]} {
+	set handle [nick2hand $nick]
+if {[string equal -nocase "action" $keyword] || [string equal -nocase "chat" $keyword]} {return}
+if {![botisop $chan] && ![setting:get $chan xonly]} { return }
+if {[matchattr $handle $black(exceptflags) $chan]} { return}
+	set bl_protect [blacktools:protect $nick $chan]
+if {$bl_protect == "1"} { return }
+if {[isbotnick $nick]} { return }
+	blacktools:banner:1 $nick "ANTICTCP" $chan $host [get:banmethod "antictcp" $chan] [link:chan:get $chan]
+	}
+}
+
+##############
+#########################################################################
+##   END                                                               ##
+#########################################################################
---
+++ BlackTools2.5.2/BlackTools/Protections/BT.antirepeat.tcl
@@ -0,0 +1,71 @@
+#########################################################################
+##          BlackTools - The Ultimate Channel Control Script           ##
+##                    One TCL. One smart Eggdrop                       ##
+#########################################################################
+########################   ANTIREPEAT TCL   #############################
+#########################################################################
+##						                       ##
+##   BlackTools  : http://blacktools.tclscripts.net	               ##
+##   Bugs report : http://www.tclscripts.net/	                       ##
+##   GitHub page : https://github.com/tclscripts/BlackToolS-TCL-script ##
+##   Online Help : irc://irc.undernet.org/tcl-help 	               ##
+##                 #TCL-HELP / UnderNet                                ##
+##                 You can ask in english or romanian                  ##
+##					                               ##
+#########################################################################
+
+proc antirepeat:protect {nick host hand chan arg} {
+global black botnick
+	set arg [color:filter [join [split $arg]]]
+	set handle [nick2hand $nick]
+if {![validchan $chan]} { return }
+if {![botisop $chan] && ![setting:get $chan xonly]} { return }
+if {[matchattr $handle $black(exceptflags) $chan]} { return }
+	set bl_protect [blacktools:protect $nick $chan]
+if {$bl_protect == "1"} { return }
+if {[isbotnick $nick]} { return }
+	set repeatset [setting:get $chan antirepeat-setting]
+if {$repeatset == ""} { set repeatset "$black(antirepeat:repeats)" }
+	set number [scan $repeatset %\[^:\]]
+	set time [scan $repeatset %*\[^:\]:%s]
+foreach tmr [utimers] {
+if {[string match "*antirepeat:remove $host $chan $arg*" [join [lindex $tmr 1]]]} {
+	killutimer [lindex $tmr 2]
+	}	
+}
+
+if {![info exists black(repeat:$host:$chan:$arg)]} { 
+	set black(repeat:$host:$chan:$arg) 0 
+}
+	incr black(repeat:$host:$chan:$arg)
+	utimer $time [list antirepeat:remove $host $chan $arg]
+
+if {$black(repeat:$host:$chan:$arg) >= $number} {
+	blacktools:banner:1 $nick "ANTIREPEAT" $chan $host [get:banmethod "antirepeat" $chan] [link:chan:get $chan]
+	antirepeat:remove $host $chan $arg
+	return 1
+	}
+}
+
+proc antirepeat:remove {host chan arg} {
+	global black
+	
+foreach tmr [utimers] {
+if {[string match "*antirepeat:remove $host $chan $arg*" [join [lindex $tmr 1]]]} {
+	killutimer [lindex $tmr 2]
+	}	
+}
+
+if {[info exists black(repeat:$host:$chan:$arg)]} {
+	unset black(repeat:$host:$chan:$arg)
+	}
+}
+
+proc antirepeat:protect:me {nick host hand chan keyword arg} {
+	antirepeat:protect $nick $host $hand $chan $arg
+}
+
+##############
+#########################################################################
+##   END                                                               ##
+#########################################################################
