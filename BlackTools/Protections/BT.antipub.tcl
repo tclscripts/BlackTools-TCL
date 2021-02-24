81fe3df97b960a10efe13da4cf9d7240146c3500
Daniel Voipan <voipandaniel@gmail.com>
2021-02-24 15:20:31 +0200

    Update BT.antipub.tcl

--- BlackTools/Protections/BT.antipub.tcl
+++ BlackTools/Protections/BT.antipub.tcl
@@ -4,14 +4,14 @@
 #########################################################################
 ##########################   ANTIPUB TCL   ##############################
 #########################################################################
-##						                      						   ##
-##   BlackTools  : http://blacktools.tclscripts.net		               ##
+##						                       ##
+##   BlackTools  : http://blacktools.tclscripts.net		       ##
 ##   Bugs report : http://www.tclscripts.net/	                       ##
-##   GitHub page : https://github.com/tclscripts/BlackToolS-TCL-script ##
-##   Online Help : irc://irc.undernet.org/tcl-help 	  	               ##
+##   GitHub page : https://github.com/tclscripts/BlackToolS-TCL	       ##
+##   Online Help : irc://irc.undernet.org/tcl-help 	  	       ##
 ##                 #TCL-HELP / UnderNet                                ##
 ##                 You can ask in english or romanian                  ##
-##					                              					   ##
+##					                               ##
 #########################################################################
 
 proc antipub:protect {nick host hand chan arg} {
