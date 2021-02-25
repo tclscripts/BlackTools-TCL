0266752439867098f9472111d7372ed9b414653e
Daniel Voipan <voipandaniel@gmail.com>
2021-02-25 12:03:46 +0200

    Update BlackTools.tcl

--- BlackTools.tcl
+++ BlackTools.tcl
@@ -1732,7 +1732,7 @@ set black(name) "BlackToolS"
 set black(author) "BLaCkShaDoW"
 set black(vers) "2.5.4"
 set black(site) "wWw.TclScriptS.NeT"
-set black(current_modif) "1614200249"
+set black(current_modif) "1614247378"
 
 	bind evnt - init-server loginpublic
 	bind time - "* * * * *" timer:login:check
