32b32151ad162d5cd8466e80d4f736e69c2dc669
BLaCkShaDoW <voipandaniel@gmail.com>
2022-11-27 22:11:11 +0200

    Update BT.MeCmds.tcl

--- BlackTools/Commands/BT.MeCmds.tcl
+++ BlackTools/Commands/BT.MeCmds.tcl
@@ -983,12 +983,14 @@ if {[matchattr $hand nmo|M $chan]} {
 	set chan1 "$chan"
 	set why [lindex [split $arg] 2]
 	set user [lindex [split $arg] 3]
+	set uhost [lindex [split $arg] 4]
 if {[regexp {^[&#]} $why] && [matchattr $hand nmo|M $chan]} {
 	set chan "$why"
 	set why [lindex [split $arg] 3]
 	set user [lindex [split $arg] 4]
+	set uhost [lindex [split $arg] 5]
 		}
-private:process $nick $host $hand $chan $chan1 $why $user $type
+private:process $nick $host $hand $chan $chan1 $why $user $uhost $type
 	}
 }
 
